6a0bb0d91dc6cff76eb04b40a800831eafb59894
christiansiegel <christiansiegel@users.noreply.github.com>
2017-07-28 18:41:05 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -9,93 +9,77 @@
 #define SHOW_VIDEO
 #define DRAW_RECTANGLE
 
-void printSymbolFrame(const zbar::Symbol &symbol, cv::Mat &frame)
-{
-    int n = symbol.get_location_size();
-    for(int i = 0; i < n; i++)
-    {
-        int x, y;
-        x = symbol.get_location_x(i);
-        y = symbol.get_location_y(i);
-        auto p1 = cv::Point(x, y);
-
-        x = symbol.get_location_x((i+1)%4);
-        y = symbol.get_location_y((i+1)%4);
-        auto p2 = cv::Point(x, y);
-
-        cv::line(frame, p1, p2, cv::Scalar(0,0,255), 3);
-    }
+void draw_rectangle(const zbar::Symbol &code, cv::Mat &frame) {
+  for (int i = 0; i < code.get_location_size(); i++) {
+    auto p1 = cv::Point(code.get_location_x(i), 
+                        code.get_location_y(i));
+    auto p2 = cv::Point(code.get_location_x((i + 1) % 4),
+                        code.get_location_y((i + 1) % 4));
+    cv::line(frame, p1, p2, cv::Scalar(0, 0, 255), 3);
+  }
 }
 
-int processFrame(zbar::ImageScanner &scanner, cv::Mat &frame)
-{
-    using namespace std;
+int detect_codes(zbar::ImageScanner &scanner, cv::Mat &frame) {
+  using namespace std;
 
 #ifdef MEASURE_TIME
-    auto start = chrono::high_resolution_clock::now();
+  auto start = chrono::high_resolution_clock::now();
 #endif
 
-    cv::Mat grey;
-    cv::cvtColor(frame, grey, CV_BGR2GRAY);
+  cv::Mat grey;
+  cv::cvtColor(frame, grey, CV_BGR2GRAY);
 
-    zbar::Image image(
-        grey.cols,              // width
-        grey.rows,              // height
-        "Y800",                 // format
-        (uchar *)grey.data,     // data
-        grey.cols * grey.rows   // length
-    );
-
-    int n = scanner.scan(image);
-
-    for(auto symbol = image.symbol_begin();
-        symbol != image.symbol_end();
-        ++symbol)
-    {
-        cout << symbol->get_data() << endl;
-
-#if defined (SHOW_VIDEO) && defined (DRAW_RECTANGLE)
-        printSymbolFrame(*symbol, frame);
-#endif
-    }
+  zbar::Image image(grey.cols,              // width
+                    grey.rows,              // height
+                    "Y800",                 // format
+                    (uchar *)grey.data,     // data
+                    grey.cols * grey.rows); // length
+                    
+  int n = scanner.scan(image);
 
 #ifdef MEASURE_TIME
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << duration << " ms" << endl;
+  auto end = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+  cout << n << " codes (" << duration.count() << " ms):" << endl;
 #endif
+
+  for (auto code = image.symbol_begin(); code != image.symbol_end(); ++code) {
+    cout << "    >> \"" << code->get_data() << "\"" << endl;
+
+#if defined(SHOW_VIDEO) && defined(DRAW_RECTANGLE)
+    draw_rectangle(*code, frame);
+#endif
+  }
+
+  return n;
+}
+
+int main() {
+
+  cv::VideoCapture capture(0);  // video camera #0
+  if (!capture.isOpened()) return -1;
+
+  zbar::ImageScanner scanner;
+  scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 0);  // disable all symbologies
+  scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);  // enable QR Code only
 
 #ifdef SHOW_VIDEO
-    cv::imshow("MyVideo", frame);
+  cv::namedWindow("video", cv::WINDOW_AUTOSIZE);
 #endif
 
-    return n;
-}
+  for (;;) {
+    cv::Mat frame;
+    capture >> frame;
+    resize(frame, frame, cv::Size(320, 180), 0, 0, cv::INTER_CUBIC);
+    
+    detect_codes(scanner, frame);
 
-int main(int argc, char* argv[])
-{
-    using namespace std;
+#ifdef SHOW_VIDEO
+    cv::imshow("video", frame);
+#endif
 
-    cv::VideoCapture capture(0); // video camera #0
-    //cv::VideoCapture capture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
+    if (cv::waitKey(30) != 0xff) break;
+  }
 
-    if (!capture.isOpened())
-        return -1;
-
-    zbar::ImageScanner scanner;
-    scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 0); // disable all symbologies
-    scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1); // enable QR Code only
-
-    for(;;)
-    {
-        cv::Mat frame;
-        capture >> frame;
-        resize(frame, frame, cv::Size(320, 180), 0, 0, cv::INTER_CUBIC);
-        processFrame(scanner, frame);
-
-        if(cv::waitKey(25) >= 0)
-            break;
-    }
-
-    return 0;
+  return 0;
 }
